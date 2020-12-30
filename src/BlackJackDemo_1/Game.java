777aa6a6dd6946338cab17afc8b8fac5e3ba1164
sooojungp96 <sooojungp96@gmail.com>
2020-12-30 23:54:25 +0900

    블랙잭 구현하기[2]/Gamer역할 구현

--- src/BlackJackDemo_1/Game.java
+++ src/BlackJackDemo_1/Game.java
@@ -1,15 +1,43 @@
 package BlackJackDemo_1;
 
+import java.util.Scanner;
+
 public class Game {
+    private static final int INIT_RECEIVE_CARD = 2;
+    private void initPhase(CardDeck cardDeck, Gamer gamer){
+        // 매직 넘버를 활용에 2장의 카드를 생성
+        System.out.println("처음 두장의 카드를 각자 뽑겠습니다");
+        for (int i = 0; i < INIT_RECEIVE_CARD; i++) {
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
+    }
+
     public void play(){
         System.out.println("=====game Start======");
-//        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Dealer dealer = new Dealer();
         Rule rule = new Rule();
         Card card = new Card();
         Gamer gamer = new Gamer();
         CardDeck cardDeck = new CardDeck();
-        System.out.println(cardDeck.toString());
+//        System.out.println(cardDeck.toString());
+        initPhase(cardDeck, gamer);
+        playingPhase(sc, cardDeck,gamer);
 
+    }
+
+    public void playingPhase(Scanner sc, CardDeck cardDeck,Gamer gamer){
+        String gamerInput;
+        while(true){
+            System.out.println("카드를 뽑겟습니까? 게임을 원하지 않으면 0을 입력하세요");
+            gamerInput = sc.nextLine();
+
+            if("0".equals(gamerInput)){
+                break;
+            }
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
     }
 }
--- src/BlackJackDemo_1/Gamer.java
+++ src/BlackJackDemo_1/Gamer.java
@@ -1,11 +1,33 @@
 package BlackJackDemo_1;
 
+import java.util.ArrayList;
 import java.util.List;
-
+/*
+    Gamer의 역할은 아래와 같습니다.
+    - 추가로 카드를 받는다.
+    - 뽑은 카드를 소유한다.
+    - 카드를 오픈한다.
+*/
 public class Gamer {
     private List<Card> cards;
-    public void receiveCard(){}
+    public Gamer(){
+        cards = new ArrayList<>();
+    }
+    public void receiveCard(Card card){
+        this.cards.add(card);
+        this.showCards();
+    }
     public List<Card> openCard(){
         return null;
+    }
+
+    public void showCards(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재보유한 카드는\n");
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+       }
+        System.out.println(sb.toString());
     }
 }
