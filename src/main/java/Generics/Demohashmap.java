a55efc0e96d7e1a194840fa654b80fa338591de5
sooojungp96 <sooojungp96@gmail.com>
2021-01-22 00:23:39 +0900

    hashMap<인자들>의 쓰임

---
+++ src/main/java/Generics/Demohashmap.java
@@ -0,0 +1,15 @@
+package Generics;
+
+import java.util.HashMap;
+
+public class Demohashmap {
+    public static void main(String[] args) {
+        //hashMap의 put 메소드에 들어가는 타입을 지정
+        HashMap<String, Order> map =new HashMap<String, Order>();
+        map.put("소정", new Order("hot-choco", 5000, 3));
+        map.put("영범", new Order("IceAmeri",4100,5));
+        System.out.println(map.get("소정").menu);
+        System.out.println(map.get("영범").menu);
+
+    }
+}
