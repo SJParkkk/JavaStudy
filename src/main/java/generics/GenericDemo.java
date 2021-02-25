785b4e76b683d3e7204e5dcc31f2371e4fe9805a
So Jung <sooojungp96@gmail.com>
2021-02-25 22:39:28 +0900

    cleanup

--- src/main/java/generics/GenericDemo.java
+++ src/main/java/generics/GenericDemo.java
@@ -3,34 +3,33 @@ package generics;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
+/**
 <Product> 로 이뤄진 ArrayList//
 class Product
 class Tv extends Product{}
 class Audio extends Product{}
-*
 * */
 public class GenericDemo {
     public static void main(String[] args) {
-        ArrayList<Product> productlist = new ArrayList<Product>();
-        productlist.add(new Product("Product 클래스"));
-        productlist.add(new Audio("Audio 클래스"));
+        ArrayList<Product> productList = new ArrayList<Product>();
+        productList.add(new Product("Product 클래스"));
+        productList.add(new Audio("Audio 클래스"));
 
-        Product p = productlist.get(0);
+        Product p = productList.get(0);
         System.out.println(p.toString());
-        Audio o = (Audio) productlist.get(1);
+        Audio o = (Audio) productList.get(1);
         System.out.println(o.toString());
         ArrayList<Product> list = new ArrayList<Product>();
         ArrayList<TV> tvlist = new ArrayList<TV>();
 //        ArrayList<Product> tvList = new ArrayList<TV>();//컴파일 에러
         List<TV> tvList11 = new ArrayList<TV>();
 
-        productlist.add(new TV("tv"));
-        productlist.add(new Audio("audio"));
+        productList.add(new TV("tv"));
+        productList.add(new Audio("audio"));
         tvlist.add(new TV("tv1"));
         tvlist.add(new TV("tv2"));
 
-        printAll(productlist);
+        printAll(productList);
 //        printAll(tvlist);// 컴파일 에러 발생
     }
     private static void printAll(ArrayList<Product> list){
@@ -39,8 +38,6 @@ public class GenericDemo {
         }
     }
 }
-
-
 
 class Product{
     String name;
